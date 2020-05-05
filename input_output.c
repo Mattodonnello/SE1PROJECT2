b24a5ca255a8bb71769662699dbb058da8cbce69
Mattodonnello <48354237+Mattodonnello@users.noreply.github.com>
2020-05-05 11:01:34 +0100

    Add function to make moves
    Players can now choose to play the game and make moves of their pieces if they desire. For now stacks cannot be moved to the top of stacks and the number of pieces on a square can exceed the limit of 5.

--- input_output.c
+++ input_output.c
@@ -51,3 +51,62 @@ void print_board(square board[BOARD_SIZE][BOARD_SIZE]){
     }
 }
 
+
+void make_move(square board[BOARD_SIZE][BOARD_SIZE], player players[PLAYERS_NUM]) {
+    int i, j, k, l;
+    int choice, validation;
+    printf("\nWould you like to play the game (ENTER 1 FOR YES, ANY OTHER NUMBER FOR NO): ");
+    scanf("%d", &validation);
+
+    while (validation == 1) {
+        for (int x = 0; x < PLAYERS_NUM; x++) {
+            printf("\n%s, please enter the row of the piece you want to move: ", players[x].players_name);
+            scanf("%d", &i);
+            printf("\n%s, Please enter the column of the piece you want to move: ", players[x].players_name);
+            scanf("%d", &j);
+            printf("\n%s, Please enter whether you would like to move the stack/piece up, down, left or right.\nEnter 1 for up, 2 for down, 3 for left or 4 for right:",players[x].players_name);
+            scanf("%d", &choice);
+
+            if (choice == 1){
+                k = i-1;
+                l = j;
+            }
+            if (choice == 2){
+                k = i+1;
+                l = j;
+            }
+            if (choice == 3){
+                k = i;
+                l = j-1;
+            }
+            if (choice == 4){
+                k = i;
+                l = j+1;
+            }
+
+            if((k==0 && (l ==0 || l==1 || l==6 || l==7)) ||
+               (k==1 && (l==0 || l==7)) ||
+               (k==6 && (l==0 || l==7)) ||
+               (k==7 && (l==0 || l==1 || l==6 || l==7)))
+            {
+                printf("ERROR, Move to invalid square");
+                break;
+            }
+
+            if (board[i][j].stack->p_color != players[x].player_color) {
+                printf("ERROR please enter a square where your color is actually present and is the top piece of the stack");
+                break;
+            }
+
+
+            push(board[i][j].stack->p_color, &board[k][l].stack);
+            pop(&board[i][j].stack);
+            print_board(board);
+        }
+    }
+
+    while (validation != 1){
+        printf("PLAYERS HAVE CHOSEN NOT TO PLAY THE GAME");
+        exit(0);
+    }
+}

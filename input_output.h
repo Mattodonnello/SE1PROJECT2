e93ac878fc76af7c9cdacd9998726ff5400bf5e9
Mattodonnello <48354237+Mattodonnello@users.noreply.github.com>
2020-05-06 19:50:24 +0100

    Add our function prototypes
    Add our missing function prototypes to our input_output.h file and add comments also.

--- input_output.h
+++ input_output.h
@@ -1,8 +1,3 @@
-
-//
-// Created by Lili on 24/03/2020.
-//
-
 #ifndef FOCUS_INPUT_OUTPUT_H
 #define FOCUS_INPUT_OUTPUT_H
 
@@ -11,6 +6,11 @@
 #include "gameinit.h"
 
 //Function to print the board
-void print_board(square board[BOARD_SIZE][BOARD_SIZE]);
-void push(color p_color, PiecePtr *top);
-void pop(PiecePtr *top);
+void print_board(square board[BOARD_SIZE][BOARD_SIZE]); // Function prototype for print_board function
+void make_move(square board[BOARD_SIZE][BOARD_SIZE], player players[PLAYERS_NUM]); // Function prototype for make_move function
+void push(color p_color, PiecePtr *top); // Function prototype for push function
+void pop(PiecePtr *top); // Function prototype for pull function
+void stacks(square board[BOARD_SIZE][BOARD_SIZE], int i, int j, int k, int l); // Function prototype for stacks function
+void over_five(square board[BOARD_SIZE][BOARD_SIZE], player players[PLAYERS_NUM], int k, int l); // Function prototype for over_five function
+int size_stack(square board[BOARD_SIZE][BOARD_SIZE], int i, int j); // Function prototype for size_stack function
+

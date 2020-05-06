5c3bc06cb1e8410b27f5a47376fe3af4a04b4163
Mattodonnello <48354237+Mattodonnello@users.noreply.github.com>
2020-05-06 19:52:33 +0100

    Add our make_move function and some comments

--- main.c
+++ main.c
@@ -1,22 +1,25 @@
-//
-// Created by Lili on 26/03/2020.
-//
-
 #include <stdio.h>
-
 #include "input_output.h"
 
-
+// Function main begins execution
 int main() {
 
     // declaration of the players and the board
     player players[PLAYERS_NUM];
     square board[BOARD_SIZE][BOARD_SIZE];
 
+    // Function to initialise our players
     initialize_players(players);
 
+    // Function to initialise our players
     initialize_board(board);
 
+    // Function to print our original board
     print_board(board);
+
+    // Function to allow players to play the game until a winner is found
+    make_move(board, players);
+
     return 0;
 }
+// Function main finishes execution
